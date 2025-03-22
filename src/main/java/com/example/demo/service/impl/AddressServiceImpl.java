25b7c182fc7504d515d51d71e4009f1c6632be1f
OmarElhagagy <oadeell21@gmail.com>
2025-03-22 15:15:43 +0200

    updated AddressServiceImpl

--- src/main/java/com/example/demo/service/impl/AddressServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AddressServiceImpl.java
@@ -25,4 +25,18 @@ public class AddressServiceImpl implements AddressService {
         this.addressRepository = addressRepository;
         this.customerRepository = customerRepository;
     }
+
+    @Override
+    public Address createAddress(Address address) {
+        // Ensure customer exists
+        if (address.getCustomer() == null || address.getCustomer().getId() == null) {
+            throw new IllegalArgumentException("Customer information required when creating an address");
+        }
+
+        Customer customer = customerRepository.findById(address.getCustomer().getId())
+            .orElseThrow(() -> new EntityNotFoundException("Customer not found with ID " + address.getCustomer().getId()));
+
+        // If this is customer's first address make it default
+        List<Address> existingAddresses = addressRepository.findByCustomerId(customer.getId());
+    }
 }
