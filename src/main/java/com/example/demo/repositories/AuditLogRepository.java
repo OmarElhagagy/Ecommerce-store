6e1a4c1ac9bd2317b2a9756d05d7e8872704c408
OmarElhagagy <oadeell21@gmail.com>
2025-03-26 23:45:06 +0200

    added repositories for auditLog (incomplete)

--- src/main/java/com/example/demo/repositories/AuditLogRepository.java
+++ src/main/java/com/example/demo/repositories/AuditLogRepository.java
@@ -1,7 +1,12 @@
 package com.example.demo.repositories;
 
 import com.example.demo.entities.AuditLog;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuditLogRepository extends JpaRepository<AuditLog, Integer> {
+    List<AuditLog>  findByUser_Id(Integer userId);
+    List<AuditLog>  findByEmployee_Id(Integer employeeId);
 }
