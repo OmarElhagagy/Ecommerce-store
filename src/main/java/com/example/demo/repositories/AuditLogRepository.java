262423c3406244e055bfea28f2b56db9c390932b
OmarElhagagy <oadeell21@gmail.com>
2025-03-27 22:17:01 +0200

    added AuditLog repository

--- src/main/java/com/example/demo/repositories/AuditLogRepository.java
+++ src/main/java/com/example/demo/repositories/AuditLogRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repositories;
 
 import com.example.demo.entities.AuditLog;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AuditLogRepository extends JpaRepository<AuditLog, Integer> {
     List<AuditLog>  findByUser_Id(Integer userId);
     List<AuditLog>  findByEmployee_Id(Integer employeeId);
+    List<AuditLog>  findByTableName(String tableName);
+    List<AuditLog>  findByRecordId(Integer recordId);
+    List<AuditLog>  findByTimestampBetween(Instant start, Instant end);
+    List<AuditLog>  findByTableNameAndRecordId(String tableName, Integer recordId);
 }
