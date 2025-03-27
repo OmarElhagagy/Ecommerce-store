36a4c0c5db39decae17710c076359b0a2467f467
OmarElhagagy <oadeell21@gmail.com>
2025-03-27 22:35:26 +0200

    added AuditLogService

---
+++ src/main/java/com/example/demo/service/AuditLogService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.entities.AuditLog;
+import java.time.Instant;
+import java.util.List;
+
+public interface AuditLogService {
+    AuditLog    createAuditLog(AuditLog auditLog);
+    List<AuditLog>  findByUserId(Integer userId);
+    List<AuditLog>  findByEmployeeId(Integer employeeId);
+    List<AuditLog>  findByTableName(String tableName);
+    List<AuditLog>  findByTimeRange(Instant start, Instant end);
+    List<AuditLog>  findByTableNameAndRecordId(String tableName, Integer recordId);
+}
