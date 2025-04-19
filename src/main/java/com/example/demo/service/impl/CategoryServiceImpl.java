8ccf6c6be253d4bbe8475a29ba309b41e97d9ae7
OmarElhagagy <oadeell21@gmail.com>
2025-04-19 22:33:13 +0200

    Initialized categoryServiceImpl

---
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.CategoryService;
+import com.example.demo.entities.Category;
+import com.example.demo.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Service
+    @Transactional(readOnly = true)
+    public List<Category> findAll() {
+    }
+}
